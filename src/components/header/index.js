07492b8f84c81b32cfed3e5e6bbd1778bf8c9c07
tcper <tcper@163.com>
2020-06-18 23:01:25 +0800

    update css

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,11 +4,10 @@ import style from './style.css';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Preact App</h1>
 		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
+			<Link activeClassName={style.active} href="/">Me</Link>
+			<Link activeClassName={style.active} href="/profile">Work</Link>
+			<Link activeClassName={style.active} href="/profile/john">Contact</Link>
 		</nav>
 	</header>
 );
